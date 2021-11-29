af0eae39c367187a784beb9d3cde3968e5413de0
Yu4iy <yurimohyliuk@gmail.com>
2021-11-29 14:57:43 +0100

    ADD: Header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<header>
+	<div class="container">
+		<input type="text">
+		<button>Cerca</button>
+	</div>
+</header>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/globals';
+header{
+	display: flex;
+	justify-content: center;
+	padding: 1rem 0;
+	background-color: cadetblue;
+	input{
+		width: 31.25rem;
+		height: 3.125rem;
+		padding: .3125rem;
+	}
+	button{
+		height: 3.125rem;
+		padding: 0 2rem;
+	}
+}
+
+</style>
